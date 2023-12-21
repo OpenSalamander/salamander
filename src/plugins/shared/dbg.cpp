32a0719207c537cee996f6efdc11c2619efa994c
Michal Várady <miko.vaji@gmail.com>
2023-12-21 15:37:38 +0100

    dbg.cpp, dbg.h: translated

--- src/plugins/shared/dbg.cpp
+++ src/plugins/shared/dbg.cpp
@@ -1,7 +1,7 @@
 ﻿// SPDX-FileCopyrightText: 2023 Open Salamander Authors
 // SPDX-License-Identifier: GPL-2.0-or-later
 // CommentsTranslationProject: TRANSLATED
-// 
+//
 //****************************************************************************
 //
 // Copyright (c) 2023 Open Salamander Authors
