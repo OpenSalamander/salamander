c05a625adf8e4bbfd0947a79213c79939c0c2bb4
Elias Bachaalany <elias.bachaalany@gmail.com>
2024-04-02 14:02:02 -0700

    added back default msbuild path + minor fix

--- src/vcxproj/build.cmd
+++ src/vcxproj/build.cmd
@@ -10,12 +10,21 @@ if "%~1"=="help" (
   goto :eof
 )
 
-:: Make sure that we are in a Developer Command Prompt for Visual Studio
-where msbuild
-if %errorlevel% neq 0 (
-  echo MSBuild.exe not found. Please open a Developer Command Prompt for Visual Studio.
-  echo.
-  goto :eof
+:: Check for MSBuild in the PATH
+where msbuild >nul 2>&1
+if %errorlevel% neq 0 set NO_MSBUILD=1
+
+:: Assume a default location (as per the authors of OpenSalamander preference)
+set MSB=C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe
+
+if not exist "%MSB%" (
+  if defined NO_MSBUILD (
+    echo MSBuild.exe not found. Please open a Developer Command Prompt for Visual Studio.
+    echo.
+    goto :eof
+  ) else (
+    set MSB=msbuild
+  )
 )
 
 if "%OPENSAL_BUILD_DIR%"=="" (
@@ -39,5 +48,5 @@ goto :eof
 :build
   echo Building %~1/%2
   if exist %3 del /q %3
-  "%MSB%" salamand.sln /t:build "/p:Configuration=%~1" /p:Platform=%2 /m:%NUMBER_OF_PROCESSORS%
+  "%msb%" salamand.sln /t:build "/p:Configuration=%~1" /p:Platform=%2 /m:%NUMBER_OF_PROCESSORS%
   exit /b
