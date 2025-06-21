8e6c08c6658b99d6c39ccf041c221a1ea9dce231
Lemi257 <solinma1@uhk.cz>
2025-06-21 13:30:56 +0200

    translated

--- src/logo.h
+++ src/logo.h
@@ -1,5 +1,6 @@
 ﻿// SPDX-FileCopyrightText: 2023 Open Salamander Authors
 // SPDX-License-Identifier: GPL-2.0-or-later
+// CommentsTranslationProject: TRANSLATED
 
 #pragma once
 
@@ -8,8 +9,8 @@
 // Splash Screen
 //
 
-BOOL SplashScreenOpen();                           // otevre splash screen okenko
-void SplashScreenCloseIfExist();                   // pokud okenko existuje, zavre ho
-BOOL ExistSplashScreen();                          // detekuje existenci splash screen okna
-HWND GetSplashScreenHandle();                      // vrati handle splash screenu, pripadne NULL pokud neexistuje
-void IfExistSetSplashScreenText(const char* text); // pokud existuje, nastavi text, ktery se hned zobrazi
+BOOL SplashScreenOpen();                           // opens splash screen window
+void SplashScreenCloseIfExist();                   // if window exists, closes it
+BOOL ExistSplashScreen();                          // detects existence of splash screen window
+HWND GetSplashScreenHandle();                      // returns handle of splash screen, or NULL if it doesn't exist
+void IfExistSetSplashScreenText(const char* text); // if it exists, sets text that will be displayed immediately
