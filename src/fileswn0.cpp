OpenSalamander/salamander

commit 5cb9ab00adb603d7ef9a3b721fa697653e24af38

details

src/fileswn0.cpp
@@ -2260,7 +2260,7 @@ void CFilesWindow::RefreshDirectory(BOOL probablyUselessRefresh, BOOL forceReloa
     TRACE_I("RefreshDirectory: " << (MainWindow->LeftPanel == this ? "left" : "right") << ": " << t_path);
 #endif // _DEBUG
 
-    // start the hourglass
+    // show wait cursor
     BOOL setWait = (GetCursor() != LoadCursor(NULL, IDC_WAIT)); // is it already waiting?
     HCURSOR oldCur;
     if (setWait)
@@ -3291,7 +3291,7 @@ void CFilesWindow::RefreshForConfig()
 {
     CALL_STACK_MESSAGE1("CFilesWindow::RefreshForConfig()");
     if (Is(ptZIPArchive))
-    { // we ensure a refresh of the archive by corrupting the archive's marker
+    { // we ensure a refresh of the archive by corrupting the archive's size
         SetZIPArchiveSize(CQuadWord(-1, -1));
     }
 

commit 708f65989b1f2cf4017906a7c7156c129046f0a2

Fixes build error introduced with https://github.com/OpenSalamander/salamander/pull/72

src/fileswn0.cpp
@@ -2297,6 +2297,7 @@ void CFilesWindow::RefreshDirectory(BOOL probablyUselessRefresh, BOOL forceReloa
     OnlyDetachFSListing = Is(ptPluginFS);
 
     // we'll backup the icon-cache for later transfer of loaded icons to the new icon-cache
+    BOOL oldIconCacheValid = IconCacheValid;
     BOOL oldInactWinOptimizedReading = InactWinOptimizedReading;
     CIconCache* oldIconCache = NULL;
     if (UseSystemIcons || UseThumbnails)