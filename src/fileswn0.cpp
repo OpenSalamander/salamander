OpenSalamander/salamander

commit 5cb9ab00adb603d7ef9a3b721fa697653e24af38

details

src/fileswn0.cpp
@@ -2260,7 +2260,7 @@ void CFilesWindow::RefreshDirectory(BOOL probablyUselessRefresh, BOOL forceReloa
     TRACE_I("RefreshDirectory: " << (MainWindow->LeftPanel == this ? "left" : "right") << ": " << t_path);
 #endif // _DEBUG
 
-    // start the hourglass
+    // show wait cursor
     BOOL setWait = (GetCursor() != LoadCursor(NULL, IDC_WAIT)); // is it already waiting?
     HCURSOR oldCur;
     if (setWait)
@@ -3291,7 +3291,7 @@ void CFilesWindow::RefreshForConfig()
 {
     CALL_STACK_MESSAGE1("CFilesWindow::RefreshForConfig()");
     if (Is(ptZIPArchive))
-    { // we ensure a refresh of the archive by corrupting the archive's marker
+    { // we ensure a refresh of the archive by corrupting the archive's size
         SetZIPArchiveSize(CQuadWord(-1, -1));
     }
 

commit 8f96c5e06898169a946c1e8b1f94b8914ea3d910

Fixes fileswn0.cpp
Bug introduced in https://github.com/OpenSalamander/salamander/pull/72

src/fileswn0.cpp
@@ -2905,7 +2905,7 @@ void CFilesWindow::RefreshDirectory(BOOL probablyUselessRefresh, BOOL forceReloa
                 break; // end of searching for selected items
         }
     }
-    if (focusFirstNewItem && i == Dirs->Count - 1) // found a new item
+    if (focusFirstNewItem && i == Files->Count - 1) // found a new item
     {
         if (!Is(ptDisk) || (Files->At(i).Attr & FILE_ATTRIBUTE_TEMPORARY) == 0) //  on disk, we ignore tmp files (they disappear immediately), see https://forum.altap.cz/viewtopic.php?t=2496
         {