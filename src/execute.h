5d0631f64667b92b681ec37bb313ddff0aa883e2
Maruska <solinma1@uhk.cz>
2025-10-02 13:14:11 +0200

    till line 125

--- src/execute.h
+++ src/execute.h
@@ -7,8 +7,8 @@
 //
 // CComboboxEdit
 //
-// Protoze je combobox vyprasenej, nelze klasickou cestou (CB_GETEDITSEL) zjistit
-// po opusteni focusu, jaka byla selection. To resi tento control.
+// The combobox loses focus and control is buggy so we can't determine the selection
+// the usual way. This helper control works around that problem.
 //
 
 class CComboboxEdit : public CWindow
@@ -41,38 +41,38 @@ extern const char* EXECUTE_DOSNAME;
 
 extern const char* EXECUTE_ENV; // Environment Variable
 
-extern const char* EXECUTE_SEPARATOR;    // Pro vlozeni separatoru do menu
-extern const char* EXECUTE_BROWSE;       // Pro vlozeni Browse commandu do menu
-extern const char* EXECUTE_HELP;         // Pro privolani helpu
-extern const char* EXECUTE_TERMINATOR;   // Pro ukonceni menu
-extern const char* EXECUTE_SUBMENUSTART; // zacatek submenu (umime jen jednu uroven)
-extern const char* EXECUTE_SUBMENUEND;   // konec submenu (umime jen jednu uroven)
+extern const char* EXECUTE_SEPARATOR;    // to insert a separator into the menu
+extern const char* EXECUTE_BROWSE;       // to insert a Browse command into the menu
+extern const char* EXECUTE_HELP;         // to call help
+extern const char* EXECUTE_TERMINATOR;   // to end menu
+extern const char* EXECUTE_SUBMENUSTART; // start of a submenu (only one level supported)
+extern const char* EXECUTE_SUBMENUEND;   // end of a submenu (only one level supported)
 
 //******************************************************************************
 //
 // CExecuteItem
 //
 
-// implicitne se po vyberu polozky nahradi vybrany text v editlajne
-// kurzor se postavi za tento text
-// a do editlajny se presune focus
-// nasledujici flagy modifikuji implicitni chovani:
-#define EIF_REPLACE_ALL 0x01 // obsah cele editlajny bude nahrazen
-#define EIF_CURSOR_1 0x02    // kurzor bude postaven jeden znak pred konec vlozeneho textu
-#define EIF_CURSOR_2 0x04    // kurzor bude postaven dva znaky pred konec vlozeneho textu
-#define EIF_VARIABLE 0x08    // vlozeny text bude ohranicen: $(text)
-#define EIF_DONT_FOCUS 0x10  // po akci nebude focus postaven do editlajny
+// by default after selecting the item the current text in the edit line is replaced,
+// the cursor is positioned after this text,
+// and the edit line receives focus.
+// The following flags modify this default behavior:
+#define EIF_REPLACE_ALL 0x01 // replace the entire contents of the edit line
+#define EIF_CURSOR_1 0x02    // place the cursor one character before the end of the inserted text
+#define EIF_CURSOR_2 0x04    // place the cursor two characters before the end of the inserted text
+#define EIF_VARIABLE 0x08    // wrap the inserted text in $(text)
+#define EIF_DONT_FOCUS 0x10  // do not move focus back to the edit line after the action
 
 struct CExecuteItem
 {
-    const char* Keyword; // string vlozeny do editlajny
-    int NameResID;       // string z resourcu, ktery bude vybalen v menu
+    const char* Keyword; // string inserted into the edit line
+    int NameResID;       // resource string displayed in the menu
     BYTE Flags;          // EIF_xxxx
 };
 
 //******************************************************************************
 //
-// Preddefinovana pole
+// Predefined arrays
 //
 
 extern CExecuteItem UserMenuArgsExecutes[];
@@ -86,43 +86,43 @@ extern CExecuteItem RegularExpressionItems[];
 
 //******************************************************************************
 //
-// Pomocne datove struktury
+// Helper data structures
 //
 
-struct CUserMenuValidationData // extra data pro validaci User Menu: pole Arguments
+struct CUserMenuValidationData // additional data used to validate User Menu: array Arguments
 {
-    BOOL UsesListOfSelNames;     // TRUE = tento parametr se pouziva (musime overit jestli neni prilis dlouhy)
-    BOOL UsesListOfSelFullNames; // TRUE = tento parametr se pouziva (musime overit jestli neni prilis dlouhy)
-    BOOL UsesFullPathLeft;       // TRUE = tento parametr se pouziva (musime overit jestli je definovan)
-    BOOL UsesFullPathRight;      // TRUE = tento parametr se pouziva (musime overit jestli je definovan)
-    BOOL UsesFullPathInactive;   // TRUE = tento parametr se pouziva (musime overit jestli je definovan)
+    BOOL UsesListOfSelNames;     // TRUE = this parameter is used; (check that it is not too long)
+    BOOL UsesListOfSelFullNames; // TRUE = this parameter is used; (check that it is not too long)
+    BOOL UsesFullPathLeft;       // TRUE = this parameter is used; (verify if it is defined)
+    BOOL UsesFullPathRight;      // TRUE = this parameter is used; (verify if it is defined)
+    BOOL UsesFullPathInactive;   // TRUE = this parameter is used; (verify if it is defined)
 
-    BOOL MustHandleItemsAsGroup;     // TRUE = s polozkami se musi pracovat jako se skupinou: ListOfSelectedNames, ListOfSelectedFullNames, FileToCompareXXX, DirToCompareXXX
-    BOOL MustHandleItemsOneByOne;    // TRUE = s polozkami se musi pracovat samostatne: FullName, Name, NamePart, ExtPart, DOSFullName, DOSName, DOSNamePart, DOSExtPart
-    int UsedCompareType;             // 0 = zatim zadny, 1 = file-left-right, 2 = file-active-inactive, 3 = dir-left-right, 4 = dir-active-inactive, 5 = kolize vice typu (nepripustne), 6 = file-or-dir-left-right, 7 = file-or-dir-active-inactive
-    BOOL UsedCompareLeftOrActive;    // TRUE = pouzita aspon jedna promenna compare-left nebo compare-active (testujeme jestli se pouzivaji oba parametry, jinak je to nesmysl)
-    BOOL UsedCompareRightOrInactive; // TRUE = pouzita aspon jedna promenna compare-right nebo compare-inactive (testujeme jestli se pouzivaji oba parametry, jinak je to nesmysl)
+    BOOL MustHandleItemsAsGroup;     // TRUE = items must be processed as a group: ListOfSelectedNames, ListOfSelectedFullNames, FileToCompareXXX, DirToCompareXXX
+    BOOL MustHandleItemsOneByOne;    // TRUE = items must be processed individually: FullName, Name, NamePart, ExtPart, DOSFullName, DOSName, DOSNamePart, DOSExtPart
+    int UsedCompareType;             // 0 = none yet, 1 = file-left-right, 2 = file-active-inactive, 3 = dir-left-right, 4 = dir-active-inactive, 5 = multiple types (invalid), 6 = file-or-dir-left-right, 7 = file-or-dir-active-inactive
+    BOOL UsedCompareLeftOrActive;    // TRUE = at least one variable compare-left or compare-active is used (we're testing if both parameters are used; otherwise it's nonsense)
+    BOOL UsedCompareRightOrInactive; // TRUE = at least one variable compare-right or compare-inactive is used (we're testing if both parameters are used; otherwise it's nonsense)
 };
 
-struct CUserMenuAdvancedData // extra data jen pro User Menu: pole Arguments
+struct CUserMenuAdvancedData // additional data used only for the User Menu: array Arguments
 {
-    // predpocitane hodnoty nekterych parametru:
-    char ListOfSelNames[USRMNUARGS_MAXLEN];     // prazdny string = prazdny nebo prilis dlouhy seznam (delsi nez USRMNUARGS_MAXLEN), rozhoduje ListOfSelNamesIsEmpty
-    BOOL ListOfSelNamesIsEmpty;                 // TRUE = prazdny ListOfSelNames
-    char ListOfSelFullNames[USRMNUARGS_MAXLEN]; // prazdny string = prazdny nebo prilis dlouhy seznam (delsi nez USRMNUARGS_MAXLEN), rozhoduje ListOfSelFullNamesIsEmpty
-    BOOL ListOfSelFullNamesIsEmpty;             // TRUE = prazdny ListOfSelFullNames
-    char FullPathLeft[MAX_PATH];                // prazdny string = neni definovana (jsme ve Findu nebo je v panelu archiv/FS)
-    char FullPathRight[MAX_PATH];               // prazdny string = neni definovana (jsme ve Findu nebo je v panelu archiv/FS)
-    const char* FullPathInactive;               // ukazuje do FullPathLeft nebo FullPathRight: prazdny string = neni definovana (jsme ve Findu nebo je v panelu archiv/FS)
-    char CompareName1[MAX_PATH];                // prvni full-name pro compare (soubor nebo adresar)
-    char CompareName2[MAX_PATH];                // druhy full-name pro compare (soubor nebo adresar)
-    BOOL CompareNamesAreDirs;                   // TRUE = CompareName1 a CompareName2 jsou adresare (jinak jde o soubory)
-    BOOL CompareNamesReversed;                  // TRUE = jmena pro compare nejsou z jednoho panelu + CompareName1 je z praveho panelu
+    // precompute values of some parameters:
+    char ListOfSelNames[USRMNUARGS_MAXLEN];     // empty string = empty or too long list (longer than USRMNUARGS_MAXLEN); see ListOfSelNamesIsEmpty
+    BOOL ListOfSelNamesIsEmpty;                 // TRUE = ListOfSelNames is empty
+    char ListOfSelFullNames[USRMNUARGS_MAXLEN]; // empty string = empty or too long list (longer than USRMNUARGS_MAXLEN); see ListOfSelFullNamesIsEmpty
+    BOOL ListOfSelFullNamesIsEmpty;             // TRUE = ListOfSelFullNames is empty
+    char FullPathLeft[MAX_PATH];                // empty string = not defined (we are in Find or the panel shows archive/FS)
+    char FullPathRight[MAX_PATH];               // empty string = not defined (we are in Find or the panel shows archive/FS)
+    const char* FullPathInactive;               // points to FullPathLeft or FullPathRight: empty string = not defined (we are in Find or the panel shows archive/FS)
+    char CompareName1[MAX_PATH];                // first full name for compare (file or directory)
+    char CompareName2[MAX_PATH];                // second full name for compare (file or directory)
+    BOOL CompareNamesAreDirs;                   // TRUE = CompareName1 and CompareName2 are directories (otherwise they're files)
+    BOOL CompareNamesReversed;                  // TRUE = names for compare come from different panels + CompareName1 is from the right panel
 };
 
 //******************************************************************************
 //
-// Vlastni funkce
+// Custom functions
 //
 
 // vybali Popupmenu s prislusnym seznamem a po zvoleni polozy vlozi text do editlajny
