768e8352f4a347d7ac8c8292a74c660039bdf607
Jan Ryšavý <janrysavy@users.noreply.github.com>
2025-10-06 09:13:26 +0200

    translated sfx7zip

--- src/sfx7zip/extract.h
+++ src/sfx7zip/extract.h
@@ -8,7 +8,7 @@ extern DWORD WINAPI ExtractArchive(LPVOID cabinet);
 extern int HandleError(int titleID, int messageID, unsigned long err, const char* fileName);
 extern int HandleErrorW(int titleID, int messageID, unsigned long err, const wchar_t* fileName);
 
-extern void RefreshProgress(unsigned long inend, unsigned long inptr, int diskSavePart); // pokud je diskSavePart==0, jde o prvni pulku progressu (rozbaleni do pameti); pri diskSavePart==1 jde o vybaleni na disk
+extern void RefreshProgress(unsigned long inend, unsigned long inptr, int diskSavePart); // if diskSavePart==0 this is the first half of progress (in-memory extraction); when diskSavePart==1 it represents writing to disk
 extern void RefreshName(unsigned char* current_file_name);
 
 extern unsigned long ProgressPos;
