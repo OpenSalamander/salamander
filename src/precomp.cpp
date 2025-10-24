7f1783cdf64c6195d19c336702ea685584abd844
bellus869 <b.stepankova03@gmail.com>
2025-10-24 11:48:58 +0200

    translated precomp.cpp

--- src/precomp.cpp
+++ src/precomp.cpp
@@ -1,20 +1,21 @@
 ﻿// SPDX-FileCopyrightText: 2023 Open Salamander Authors
 // SPDX-License-Identifier: GPL-2.0-or-later
+// CommentsTranslationProject: TRANSLATED
 
 #include "precomp.h"
 
-// projekt Salamander obsahuje ctyri skupiny modulu
+// The Salamander project contains four groups of modules
 //
-// 1) modul precomp.cpp, ktery postavi salamand.pch (/Yc"precomp.h")
-// 2) moduly vyuzivajici salamand.pch (/Yu"precomp.h")
-// 3) shelext.c, ktery nepouziva predkompilovane headry
-// 4) commony a tasklist.cpp maji vlastni, automaticky generovany
+// 1) module precomp.cpp which builds salamand.pch (/Yc"precomp.h")
+// 2) modules using salamand.pch (/Yu"precomp.h")
+// 3) shelext.c which does not use precompiled headers
+// 4) common files and tasklist.cpp have their own automatically generated
 //    WINDOWS.PCH (/YX"windows.h" /Fp"$(OutDir)\WINDOWS.PCH")
 
-/* FIXME_X64 - projit printf specifikatory s ohledem na x64
-Petr:  ok, to zni celkem osklive ... asi by stacilo projit %d, %x, %X, %u, %i, %o ... stejne sileny
-nejlepe s regularem co muze byt vsechno mezi (specifikace sirky, atd.) ... aby to naslo i %08X, atd.
-to bude hnuj na par dni prohrabovani
+/* FIXME_X64 - go through printf specifiers with regard to x64
+Petr:  ok, that sounds rather nasty ... it might be enough to check %d, %x, %X, %u, %i, %o ... the same madness
+preferably with a regex that covers everything in between (width specification, etc.) ... so it also finds %08X, etc.
+this will be a mess, a few days of digging through it
 ----
 http://msdn.microsoft.com/en-us/library/tcxf1dw6%28v=vs.71%29.aspx
 */
