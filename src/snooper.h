05743d2b074f9048f41b40b0d4f4f1c24663aeb2
bellus869 <b.stepankova03@gmail.com>
2026-03-14 15:24:46 +0100

    translated snooper.h

--- src/snooper.h
+++ src/snooper.h
@@ -1,25 +1,26 @@
-﻿// SPDX-FileCopyrightText: 2023 Open Salamander Authors
-// SPDX-License-Identifier: GPL-2.0-or-later
+﻿    // SPDX-FileCopyrightText: 2023 Open Salamander Authors
+    // SPDX-License-Identifier: GPL-2.0-or-later
+    // CommentsTranslationProject: TRANSLATED
 
-#pragma once
+    #pragma once
 
-class CFilesWindow;
+    class CFilesWindow;
 
-extern HANDLE RefreshFinishedEvent;
-extern int SnooperSuspended;
+    extern HANDLE RefreshFinishedEvent;
+    extern int SnooperSuspended;
 
-void AddDirectory(CFilesWindow* win, const char* path, BOOL registerDevNotification);                           // novy adresar pro cmuchala
-void ChangeDirectory(CFilesWindow* win, const char* newPath, BOOL registerDevNotification);                     // zmena zadaneho adresare
-void DetachDirectory(CFilesWindow* win, BOOL waitForHandleClosure = FALSE, BOOL closeDevNotifification = TRUE); // uz neni treba cmuchat
+    void AddDirectory(CFilesWindow* win, const char* path, BOOL registerDevNotification);                           // add a new directory for the snooper
+    void ChangeDirectory(CFilesWindow* win, const char* newPath, BOOL registerDevNotification);                     // change of the specified directory
+    void DetachDirectory(CFilesWindow* win, BOOL waitForHandleClosure = FALSE, BOOL closeDevNotifification = TRUE); // no need to search anymore
 
-BOOL InitializeThread();
-void TerminateThread();
+    BOOL InitializeThread();
+    void TerminateThread();
 
-void BeginSuspendMode(BOOL debugDoNotTestCaller = FALSE);
-void EndSuspendMode(BOOL debugDoNotTestCaller = FALSE);
+    void BeginSuspendMode(BOOL debugDoNotTestCaller = FALSE);
+    void EndSuspendMode(BOOL debugDoNotTestCaller = FALSE);
 
-typedef TDirectArray<CFilesWindow*> CWindowArray; // (CFilesWindow *)
-typedef TDirectArray<HANDLE> CObjectArray;        // (HANDLE)
+    typedef TDirectArray<CFilesWindow*> CWindowArray; // (CFilesWindow *)
+    typedef TDirectArray<HANDLE> CObjectArray;        // (HANDLE)
 
-extern CWindowArray WindowArray; // shodne indexovana pole
-extern CObjectArray ObjectArray; // k ObjectHandlu patri MainWindow
+    extern CWindowArray WindowArray; // arrays share the same indices
+    extern CObjectArray ObjectArray; // the object handle associated with the MainWindow
