b71a44dce035bfcbb235135b8819db0b4aaef1a1
Jan Ryšavý <janrysavy@users.noreply.github.com>
2026-05-06 10:06:39 +0200

    [translation] Fix src/plugins/uniso/udf.cpp comments (#1373)

--- src/plugins/uniso/udf.cpp
+++ src/plugins/uniso/udf.cpp
@@ -513,7 +513,7 @@ int CUDF::ReadFileEntry(Uint8* data, bool bEFE, Uint16 part, CICBTag* icbTag, CU
                 ad->Length = l_ad;
                 ad->Location = 0; // To be determined by the caller
                 ad->Partition = part;
-                return -1; // -1 tells the caller that Location is not set yet
+                return -1; // -1 tells the caller the data is inlined, but Location is not set
             }
         }
         else
@@ -1181,7 +1181,7 @@ int CUDF::UnpackFile(CSalamanderForOperationsAbstract* salamander, const char* s
                 // picb->Offset is nonzero only for small (less than a sector) files inlined within File Entry
                 if (!file.Write(sector + picb->Offset, nbytes, &written, name, NULL))
                 {
-                    // The error message was already displayed.
+// The error message was already displayed by SafeWriteFile().
                     ret = UNPACK_CANCEL;
                     bFileComplete = FALSE;
                     break;
