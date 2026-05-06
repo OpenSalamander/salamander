1838cfbc58c6b320999d840563bee546ae2c07aa
Jan Ryšavý <janrysavy@users.noreply.github.com>
2026-05-06 10:22:36 +0200

    [translation] Fix src/plugins/ftp/fs1.cpp comments (#1351)

--- src/plugins/ftp/fs1.cpp
+++ src/plugins/ftp/fs1.cpp
@@ -39,7 +39,7 @@ void WINAPI HTMLHelpCallback(HWND hWindow, UINT helpID)
 
 BOOL InitFS()
 {
-#ifdef _DEBUG // must be 4 bytes because it is stored in LastWrite into the upper and lower DWORD
+#ifdef _DEBUG // must be 4 bytes because it is stored in LastWrite in the upper and lower DWORDs
     if (sizeof(CFTPTime) != 4 || sizeof(CFTPDate) != 4)
         TRACE_E("FATAL ERROR: sizeof(CFTPTime) or sizeof(CFTPDate) is not 4 bytes!");
 #endif
@@ -215,7 +215,7 @@ void ReleaseFS()
         SalamanderGeneral->DestroySafeWaitWindow();
     }
 
-    // kill auxiliary threads that did not finish "legally"
+    // kill auxiliary threads that did not finish cleanly
     AuxThreadQueue.KillAll(TRUE, 0, 0);
 
     if (FTPIcon != NULL)
